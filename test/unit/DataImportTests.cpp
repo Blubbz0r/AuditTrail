f92056d8ab7a5fc44a824428d4ca9da373254c0a
Blubbz0r <r.habrich@googlemail.com>
2015-09-26 16:04:27 +0200

    Added test for importing user node of DataImport message

--- test/unit/DataImportTests.cpp
+++ test/unit/DataImportTests.cpp
@@ -11,6 +11,9 @@ class DataImportTests : public Test
 public:
     void checkEventIdentification(const Node& eventIdentification);
     void checkEventId(const Node& eventId);
+
+    void checkImportingUser(const Node& importingUser);
+    void checkRoleIDCodeImportingUser(const Node& roleIDCode);
 };
 
 TEST_F(DataImportTests, createNodes_WithAllAttributes_ReturnsCorrectNodes)
@@ -19,11 +22,15 @@ TEST_F(DataImportTests, createNodes_WithAllAttributes_ReturnsCorrectNodes)
 
     auto nodes = dataImport.createNodes();
 
-    ASSERT_THAT(nodes.size(), Eq(1));
+    ASSERT_THAT(nodes.size(), Eq(2));
 
     auto node = nodes[0];
     ASSERT_THAT(node.name(), Eq("EventIdentification"));
     checkEventIdentification(node);
+
+    node = nodes[1];
+    ASSERT_THAT(node.name(), Eq("ActiveParticipant"));
+    checkImportingUser(node);
 }
 
 void DataImportTests::checkEventIdentification(const Node& eventIdentification)
@@ -64,4 +71,40 @@ void DataImportTests::checkEventId(const Node& eventId)
     attribute = eventId.attributes().at(2);
     ASSERT_THAT(attribute.name, Eq("displayName"));
     EXPECT_THAT(attribute.value, Eq("Import"));
+}
+
+void DataImportTests::checkImportingUser(const Node& importingUser)
+{
+    ASSERT_THAT(importingUser.attributes().size(), Eq(2));
+
+    auto attribute = importingUser.attributes().at(0);
+    ASSERT_THAT(attribute.name, Eq("UserID"));
+    EXPECT_THAT(attribute.value, Eq("john.doe@gmail.com"));
+
+    attribute = importingUser.attributes().at(1);
+    ASSERT_THAT(attribute.name, Eq("UserIsRequestor"));
+    EXPECT_THAT(attribute.value, Eq("true"));
+
+    ASSERT_THAT(importingUser.nodes().size(), Eq(1));
+
+    auto node = importingUser.nodes().at(0);
+    ASSERT_THAT(node.name(), Eq("RoleIDCode"));
+    checkRoleIDCodeImportingUser(node);
+}
+
+void DataImportTests::checkRoleIDCodeImportingUser(const Node& roleIDCode)
+{
+    ASSERT_THAT(roleIDCode.attributes().size(), Eq(3));
+
+    auto attribute = roleIDCode.attributes().at(0);
+    ASSERT_THAT(attribute.name, Eq("code"));
+    EXPECT_THAT(attribute.value, Eq("110152"));
+
+    attribute = roleIDCode.attributes().at(1);
+    ASSERT_THAT(attribute.name, Eq("codeSystemName"));
+    EXPECT_THAT(attribute.value, Eq("DCM"));
+
+    attribute = roleIDCode.attributes().at(2);
+    ASSERT_THAT(attribute.name, Eq("displayName"));
+    EXPECT_THAT(attribute.value, Eq("Destination Role ID"));
 }
